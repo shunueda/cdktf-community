d729bb5e8826e4fb27e81c683c1b1772c831be82
Shun Ueda <me@shu.nu>
2025-10-28 01:47:31 -0400

    build: no concurrency

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,20 @@
 import { get } from '@cdktf/cli-core'
 import { Language, TerraformProviderConstraint } from '@cdktf/commons'
 import { join } from 'node:path'
-import { limitFunction } from 'p-limit'
 import { fetchProviders } from './fetch-providers.ts'
 
 const providers = await Array.fromAsync(fetchProviders())
 
-await Promise.all(
-  new Set(providers).values().map(
-    limitFunction(
-      async name => {
-        try {
-          await get({
-            constraints: [new TerraformProviderConstraint(name)],
-            constructsOptions: {
-              targetLanguage: Language.TYPESCRIPT,
-              codeMakerOutput: join('gen', name)
-            }
-          })
-        } catch (e) {
-          console.warn(`Failed to generate provider ${name}: ${e}`)
-        }
-      },
-      // pulled out of thin air; could be adjusted, but it seems to work fine
-      // limit too high causes out of disk space errors on GitHub Actions
-      { concurrency: 5 }
-    )
-  )
-)
+for (const provider of new Set(providers)) {
+  try {
+    await get({
+      constraints: [new TerraformProviderConstraint(provider)],
+      constructsOptions: {
+        targetLanguage: Language.TYPESCRIPT,
+        codeMakerOutput: join('gen', provider)
+      }
+    })
+  } catch (e) {
+    console.warn(`Failed to generate provider ${provider}: ${e}`)
+  }
+}
