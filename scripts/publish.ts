fc8c8c7a030d4d07496cce00181eb176f375ecdc
Shun Ueda <me@shu.nu>
2025-10-29 00:31:33 -0400

    feat: publish to npm

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,11 +1,14 @@
+import { Arborist } from '@npmcli/arborist'
 import { build } from 'esbuild'
+import { publish } from 'libnpmpublish'
 import { type PathLike } from 'node:fs'
 import { glob, readdir, readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
+import { env } from 'node:process'
+import pacote from 'pacote'
 import { config } from '../src/config.ts'
 import { createPackageJson } from '../src/package-json.ts'
 import { semverStringSchema } from '../src/schema.ts'
-import { createTsconfigJson } from '../src/tsconfig-json.ts'
 
 async function parseVersion(file: PathLike): Promise<string> {
   const content = await readFile(file, 'utf-8')
@@ -14,16 +17,28 @@ async function parseVersion(file: PathLike): Promise<string> {
   return semverStringSchema.parse(version)
 }
 
+async function isVersionPublished(
+  pkgname: string,
+  version: string
+): Promise<boolean> {
+  try {
+    await pacote.manifest(`${pkgname}@${version}`)
+    return true
+  } catch {
+    return false
+  }
+}
+
 for (const namespace of await readdir(config.genDir)) {
   for (const name of await readdir(join(config.genDir, namespace))) {
     // add `name-` prefix to follow the official cdktf convension.
     // if namespace is same as name, do not duplicate the name.
-    const pkgname = [
+    const pkgname = `@cdktf-community/${[
       'provider',
       ...(namespace === name ? [name] : [namespace, name])
     ]
       .join('-')
-      .toLowerCase()
+      .toLowerCase()}`
 
     const dir = join(config.genDir, namespace, name)
 
@@ -45,13 +60,21 @@ for (const namespace of await readdir(config.genDir)) {
 
     await writeFile(
       join(dir, 'package.json'),
-      JSON.stringify(
-        createPackageJson(`@cdktf-community/${pkgname}`, version, dir)
-      )
+      JSON.stringify(createPackageJson(pkgname, version, dir))
     )
-    await writeFile(
-      join(dir, 'tsconfig.json'),
-      JSON.stringify(createTsconfigJson(name))
-    )
+
+    if (await isVersionPublished(pkgname, version)) {
+      continue
+    }
+    console.log('Publishing', pkgname, version)
+    const manifest = await pacote.manifest(dir)
+    const tarData = await pacote.tarball(dir, { Arborist })
+    console.log(tarData)
+    await publish(manifest as any, tarData, {
+      provenance: true,
+      token: env.NPM_TOKEN
+    })
+    break
   }
+  break
 }
